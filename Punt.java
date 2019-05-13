b857f65973dbd8300194ff156d549f2a77bad41e
Gastón Rossi <cf18gaston.rossi@iesjoandaustria.org>
2019-05-13 16:09:31 +0200

    Ejercicio 7 resuelto. Coord[]

--- Punt.java
+++ Punt.java
@@ -1,31 +1,31 @@
 public class Punt {
 	public Punt(int x, int y) {
-		this.x = x;
-		this.y = y;
+		this.coord[0] = x;
+		this.coord[1] = y;
 	}
 
 	public Punt() {
 		this(0, 0);
 	}
 
-	private int x, y; // coordenades del punt
+	private int[] coord = new int[2]; // coordenades del punt
 
 	public int getX() {
-		return x;
+		return coord[0];
 	}
 
 	public int getY() {
-		return y;
+		return coord[1];
 	}
 
 	public void setX(int x) {
-		this.x = x;
+		this.coord[0] = x;
 	}
 
 	public void setY(int y) {
-		this.y = y;
+		this.coord[1] = coord[1];
 		System.out.println("Set y");
 	}
 
-    public void suma(Punt p) { x+=p.x; y+=p.y; }
+    public void suma(Punt p) { coord[0]+=p.coord[0]; coord[1]+=p.coord[2]; }
 }
--- Segment.java
+++ Segment.java
@@ -1,12 +1,12 @@
 
 public class Segment {
-	private double p1X, p1Y, p2X, p2Y;
+	private double[] coord = new double[4];
 	
 	public Segment(double p1X, double p1Y, double p2X, double p2Y) {
-		this.setP1X(p1X);
-		this.setP1Y(p1Y);
-		this.setP2X(p2X);
-		this.setP2Y(p2Y);
+		this.coord[0] = p1X;
+		this.coord[1] = p1Y;
+		this.coord[2] = p2X;
+		this.coord[3] = p2Y;
 	}
 	
 	public Segment() {
@@ -19,35 +19,35 @@ public class Segment {
 	}
 
 	public double getP1X() {
-		return p1X;
+		return coord[0];
 	}
 
 	public void setP1X(double p1x) {
-		p1X = p1x;
+		coord[1] = p1x;
 	}
 
 	public double getP1Y() {
-		return p1Y;
+		return coord[1];
 	}
 
 	public void setP1Y(double p1y) {
-		p1Y = p1y;
+		coord[1] = p1y;
 	}
 
 	public double getP2X() {
-		return p2X;
+		return coord[2];
 	}
 
 	public void setP2X(double p2x) {
-		p2X = p2x;
+		coord[2] = p2x;
 	}
 
 	public double getP2Y() {
-		return p2Y;
+		return coord[3];
 	}
 
 	public void setP2Y(double p2y) {
-		p2Y = p2y;
+		coord[3] = p2y;
 	}
 
 	
